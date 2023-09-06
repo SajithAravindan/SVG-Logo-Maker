12db62ba3633667e777d73c66a85956e111b879a
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-09-06 18:23:23 -0400

    fixed error in the  test files

--- tests/square.test.js
+++ tests/square.test.js
@@ -1,16 +1,16 @@
 const Square = require('../lib/square.js');
 
-//test case for class square
+//test case for class circle
 describe('Square', () => {
     it('given user choice of Logo shape color, Logo text, and Logo text color, should return logo svg file with ', () => {
-        const square = new Square('green','SVG','white');
+        const square = new Square('green', 'SVG', 'white');
         expect(square.render()).toEqual(`
         <svg version="1.1"
-            width="300" height="200"
-            xmlns="http://www.w3.org/2000/svg">           
-            <rect width="200" height="200" fill="green"/>
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect width="200" height="200" fill="green"/>
         <text x="100" y="125" font-size="70" text-anchor="middle" fill="white">SVG</text>
-        </svg>
+    </svg>
         `
         );
     });
